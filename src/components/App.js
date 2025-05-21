4e05746b8e7590022b1400ccdfa5eb5d3b45197e
Vishnu <bandarivishnu143@gmail.com>
2025-05-21 17:24:01 +0530

    vis

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,48 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+// import './styles.App.css';
 
+const App = () => {
+    const [Email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+  const data = [
+    {
+      id: 1,
+      name: "ABC",
+      email: "abc@gmail.com",
+      password: "12",
+    },
+    {
+      id: 2,
+      name: "DEF",
+      email: "def@gmail.com",
+      password: "1234",
+    },
+    {
+      id: 3,
+      name: "GHI",
+      email: "ghi@gmail.com",
+      password: "123456",
+    },
+  ];
+  return (
+    <div className="main">
+      <label >Email:</label>
+      <input value={Email} onChange={(e)=>setEmail(e.target.value)} type="text" placeholder="Email" />
+      <label>passeord:</label>
+      <input value={password} onChange={(e)=>setPassword(e.target.value)} type="passeord" placeholder="password" />
+      <button onClick={()=>{
+        const user = data.find((user) => user.email === Email && user.password === password);
+        if (user) {
+          console.log("Login successful");
+        } else {
+          console.log("Invalid email or password");
+        }
+        setEmail("");
+        setPassword("");
+      }}>submit</button>
+
+    </div>
+  );
+};
+
+export default App;
